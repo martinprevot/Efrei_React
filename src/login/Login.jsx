3e29bc7e731b466165dd69384032ff9227aa8527
Daniel MONTEIRO <101821091+DanielDSM23@users.noreply.github.com>
2024-06-12 15:39:45 +0200

    Auth

---
+++ src/login/Login.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import users from "./users";
+
+const Login = () => {
+    const [studentNumber, setStudentNumber] = useState('');
+    const [password, setPassword] = useState('');
+    const submitEvent = () =>{
+        let userMatching = null;
+        for(let i = 0; i<users.length; i++){
+            if(users[i].username == studentNumber){
+                userMatching = i;
+                if(users[userMatching].password == password){
+                    console.log("ok");
+                }
+                else{
+                    console.log("nope");
+                }
+            }
+        }
+        if(userMatching == null){
+            console.log("nope");
+        }
+    }
+    
+    return (
+    <div>
+        <input type="id" placeholder="Enter votre numéro etudiant"  onChange={(e) => setStudentNumber(e.target.value)}/>
+        <br></br>
+        <input type="password" placeholder="Enter votre mot de passe"  onChange={(e) => setPassword(e.target.value)}/>
+        <br></br>
+        <input type="submit" value="Se connecter" onClick={submitEvent}/>
+
+    </div>
+
+
+)}
+
+export default Login;
---
+++ src/login/users.js
@@ -0,0 +1,11 @@
+let users = [
+    {
+        username : "12345678", 
+        password : "1234",
+        name : "Daniel MONTEIRO", 
+        role : "STUDENT"
+    }
+
+]
+
+export default users
